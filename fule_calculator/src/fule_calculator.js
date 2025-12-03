3b361e303bc13e168d00999fb513324eec219786
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-12-03 11:48:08 +0530

    Part 1 finished

--- fule_calculator/src/fule_calculator.js
+++ fule_calculator/src/fule_calculator.js
@@ -1,3 +1,8 @@
 const divideBy3AndRound = (mass) => Math.floor(mass / 3);
 
 export const calculateEach = (mass) => divideBy3AndRound(mass) - 2;
+
+export const calculateFule = (mass) => {
+  const masses = mass.split("\n");
+  return masses.reduce((sum, mass) => sum += calculateEach(parseInt(mass)), 0);
+};
--- fule_calculator/test/fule_calculator_test.js
+++ fule_calculator/test/fule_calculator_test.js
@@ -1,6 +1,23 @@
 import { assertEquals } from "@std/assert";
-import { calculateEach } from "../src/fule_calculator.js";
+import { calculateFule } from "../src/fule_calculator.js";
 
 Deno.test("Sample test", () => {
-  assertEquals(calculateEach(12), 2);
+  assertEquals(calculateFule("12"), 2);
 });
+
+Deno.test("Sample test", () => {
+  assertEquals(calculateFule("14"), 2);
+});
+
+Deno.test("Sample test", () => {
+  assertEquals(calculateFule("1969"), 654);
+});
+
+Deno.test("Sample test", () => {
+  assertEquals(calculateFule("100756"), 33583);
+});
+
+Deno.test("Input data", () => {
+  const data = Deno.readTextFileSync('./src/input_data.txt');
+  assertEquals(calculateFule(data), 3125750);
+})
