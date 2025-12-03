e4fac15fb9e0b474e6fb62be96b27a022bb0de80
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-12-03 13:57:59 +0530

    Day 2 - part 1

--- sprint/test/sprint_test.js
+++ sprint/test/sprint_test.js
@@ -1,4 +1,10 @@
 import { assertEquals } from "@std/assert";
 import { sprint } from "../src/sprint.js";
 
+Deno.test("Sample test", () => {
+  assertEquals(sprint("1,0,0,0,99"), 2);
+});
 
+// Deno.test("Sample test", () => {
+//   assertEquals(sprint("2,3,0,3,99"), 6);
+// });
