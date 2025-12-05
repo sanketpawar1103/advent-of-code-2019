898ede4f9523344c613408f0f93158b9cc8ba607
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-12-05 15:07:31 +0530

    File name change

--- day_03_crossed_wires.js
+++ day_03_crossed_wires.js
@@ -10,18 +10,23 @@ const wire2 = {
   path: [],
 };
 
-const move = (wire, axis, steps, value) => {
-  for (let step = 0; step < steps; step++) {
-    wire[axis] += value;
-    wire.path.push([wire.x, wire.y]);
-  }
+const move = (wire, axis, steps) => {
+  const crrtPos = [wire.x, wire.y];
+  wire[axis] += steps;
+  const newPos = [wire.x, wire.y];
+
+  wire.path.push([crrtPos, newPos]);
+  // for (let step = 0; step < steps; step++) {
+  //   wire[axis] += value;
+  //   wire.path.push([wire.x, wire.y]);
+  // }
 };
 
 const commands = {
-  U: (wire, steps) => move(wire, "y", steps, 1),
-  D: (wire, steps) => move(wire, "y", steps, -1),
-  R: (wire, steps) => move(wire, "x", steps, 1),
-  L: (wire, steps) => move(wire, "x", steps, -1),
+  U: (wire, steps) => move(wire, "y", +steps),
+  D: (wire, steps) => move(wire, "y", -steps),
+  R: (wire, steps) => move(wire, "x", +steps),
+  L: (wire, steps) => move(wire, "x", -steps),
 };
 
 const start = (path, wire) => {
@@ -30,15 +35,21 @@ const start = (path, wire) => {
   }
 };
 
-const areEqual = (point1, point2) =>
+const isInRange = (point1, point2) =>
   point1[0] === point2[0] && point1[1] === point2[1];
 
 const matchIntersections = (path1, path2) => {
+  // for (let index = 0; index < path1.length; index++) {
+  //   for (let index = 0; index < array.length; index++) {
+  //     const element = array[index];
+      
+  //   }
+  // }
   const intersections = [];
 
   for (let i = 0; i < path1.length; i++) {
     for (let j = 0; j < path2.length; j++) {
-      if (areEqual(path1[i], path2[j])) {
+      if (isInRange(path1[i], path2[j])) {
         intersections.push(path2[j]);
       }
     }
@@ -55,15 +66,19 @@ const findDistance = (intersectionPoints) =>
 const crossedWires = (path1, path2) => {
   start(path1, wire1);
   start(path2, wire2);
+
+  console.log(wire1.path);
+  console.log(wire2.path);
+
   const intersectionPoints = matchIntersections(wire1.path, wire2.path);
-  console.log(Math.min(...findDistance(intersectionPoints)));
+  // console.log(Math.min(...findDistance(intersectionPoints)));
 };
 
 const splitPaths = (path, delimiter) => path.split(delimiter);
 
 const main = () => {
   const paths = splitPaths(
-    Deno.readTextFileSync("./crosed_wire_data.txt"),
+    "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83",
     "\n",
   );
 
@@ -71,3 +86,6 @@ const main = () => {
 };
 
 main();
+
+const part2 = () => {
+};
