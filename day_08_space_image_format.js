54f86ac4295d4dd23c7b5dd55be8abf3c3649899
Sanket Pawar <sanketpawar@Sankets-MacBook-Pro.local>
2025-12-13 20:29:20 +0530

    Adds part 2

--- day_08_space_image_format.js
+++ day_08_space_image_format.js
@@ -1,22 +1,30 @@
 import { chunk } from "jsr:@std/collections";
 
-const occurances = (layer, pix) =>
-  layer.filter((element) => element === pix).length;
+const non2s = (col, layers, row, emptyArr) => {
+  const firstNon2s = emptyArr;
+  console.log(layers.length);
 
-const leastZeros = (layers) => {
-  const zeros = layers.map((layer) => occurances(layer, "0"));
-  const leastZeros = Math.min(...zeros);
+  while (col < layers[0].length) {
+    if (layers[row][col] !== "2") {
+      firstNon2s.push(layers[row][col]);
+      [col, row] = [col + 1, -1];
+    }
 
-  return zeros.findIndex((element) => element === leastZeros);
+    row = (row + 1) % layers.length;
+  }
+
+  return firstNon2s;
 };
 
 const main = () => {
   const str = Deno.readTextFileSync("./day_08_space_image_format_data.txt");
   const layers = chunk(str, 25 * 6);
 
-  const layerIndex = leastZeros(layers);
-  return occurances(layers[layerIndex], "1") *
-    occurances(layers[layerIndex], "2");
+  const allNon2s = non2s(0, layers, 0, []);
+
+  const string = allNon2s.map((pix) => +pix ? "⬜️" : "⬛️");
+
+  console.log(chunk(string, 25).map((each) => each.join("")).join("\n"));
 };
 
 console.log(main());
